a264c3d9cfe30d583208e1fbc376c20c51a8c18c
sbrace <sbrace0822@gmail.com>
2023-12-08 10:33:28 +0800

    init

---
+++ src/main/java/com/example/testredis/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.testredis.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @RequestMapping("/echo")
+    public String echo(String echo) {
+        return echo;
+    }
+}
